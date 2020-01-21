9f82c56a9f150709758a92c964411cd16c507374
alumno <alumno@E307>
2020-01-21 08:49:09 -0700

    Figuritias :D

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -14,6 +14,85 @@
 
 using namespace std;
 
+
+void dibujarPoligono() {
+	glBegin(GL_POLYGON);
+	glColor3f(0.4f, 0.4f, 0.9f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.5f, 0.0f);
+	glVertex3f(0.2f, 0.3f, 0.0f);
+	glVertex3f(0.6f, 0.2f, 0.0f);
+	glEnd();
+}
+
+void dibujarTrianguloContinuo() {
+	glBegin(GL_TRIANGLE_STRIP);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.1f, 0.1f, 0.0f );
+	glVertex3f(0.2f, 0.0f, 0.0f);
+
+	glColor3f(1.0f, 0.2f, 0.8f);
+	glVertex3f(0.6f, 0.2f, 0.0f);
+
+	glEnd();
+}
+
+
+void dibujarLineaContinua() {
+	glBegin(GL_LINE_STRIP);
+
+	glColor3f(0.1f, 0.3f, 0.75f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.2f, 0.0f);
+	glVertex3f(0.2f, 0.8f, 0.0f);
+	glVertex3f(0.7f, 0.1f, 0.0f);
+
+	glEnd();
+}
+
+
+void dibujarLineas() {
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.4f, 0.6f);
+
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.2f, -0.4f, 0.0f);
+
+	glVertex3f(-0.3f, 0.1f, 0.0f);
+	glVertex3f(0.2f, 0.0f, 0.0f);
+
+	glEnd();
+}
+
+void dibujarTriangulos() {
+	//Establecemos el tipo de primitiva
+	glBegin(GL_TRIANGLES);
+	//Enviar vertices X, Y, Z... Establecemos el color 
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+
+
+
+	//Especificar que dejaremos de dibujar
+	glEnd();
+}
+
+
+void dibujar() {
+	dibujarPoligono();
+}
+
 int main()
 {
 
@@ -29,7 +108,7 @@ int main()
 	//	Si se pudo iniciar el GLFW
 	//Inicializamos la ventana
 
-	window = glfwCreateWindow(800, 600, "ventana", NULL, NULL);
+	window = glfwCreateWindow(600, 600, "ventana", NULL, NULL);
 
 	//Si no se pudo crear la ventana terminamos la ejecucion
 	//Terminamos la ejecucion
@@ -55,15 +134,18 @@ int main()
 		//Ciclo de dibujo (Draw loop)
 		while (!glfwWindowShouldClose(window)) {
 			//Establecer region de dibujo 
-			glViewport(0, 0, 800, 600);
+			glViewport(0, 0, 600, 600);
 			//Establecemos el color de borrado
 			//Valores RGBA
 			glClearColor(1, 0.8, 0, 1);
 			//Borrar
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+			
 			//Actualizar valores y dibujar
 
+			dibujar();
+
+			glfwPollEvents();
 			glfwSwapBuffers(window);
 		}
 		//Despues del ciclo de dibujo
